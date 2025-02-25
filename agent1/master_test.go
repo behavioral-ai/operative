38ae383c276e7e03b8682e389716ae8c6e7cb0a8
advanced-go <mark.wilson.go@outlook.com>
2025-02-25 15:00:21 -0600

    Testing

--- agent1/master_test.go
+++ agent1/master_test.go
@@ -1,13 +1,16 @@
 package agent1
 
 import (
+	"github.com/behavioral-ai/core/iox"
 	"github.com/behavioral-ai/core/messaging"
 	"github.com/behavioral-ai/domain/collective"
 	"github.com/behavioral-ai/domain/common"
+	"github.com/behavioral-ai/operative/testrsc"
+	"github.com/behavioral-ai/operative/urn"
 	"time"
 )
 
-func ExampleMaster() {
+func _ExampleMaster() {
 	ch := make(chan struct{})
 	agent := newOp(nil, common.Origin{Region: "us-west"}, messaging.Notify, messaging.NewTraceDispatcher(), 0)
 
@@ -36,10 +39,15 @@ func ExampleMaster_Observation() {
 	agent := newOp(nil, origin, messaging.Notify, messaging.NewTraceDispatcher(), 0)
 	msg := messaging.NewMessage(messaging.MasterChannel, messaging.ObservationEvent)
 	msg.SetContent(contentTypeObservation, observation{origin: origin, latency: 2350, gradient: 15})
+	resolver, status := createResolver()
+	if !status.OK() {
+		messaging.Notify(status)
+	}
 
 	go func() {
-		go masterAttend(agent, collective.NewEphemeralResolver("", nil, true))
+		go masterAttend(agent, resolver)
 		agent.Message(msg)
+		time.Sleep(testDuration * 2)
 
 		agent.Message(messaging.MasterShutdown)
 		time.Sleep(testDuration)
@@ -52,4 +60,21 @@ func ExampleMaster_Observation() {
 	//Output:
 	//test: masterAttend() -> [finalized:true]
 
+}
+
+func createResolver() (collective.Resolution, *messaging.Status) {
+	resolver := collective.NewEphemeralResolver("", nil, true)
+	buf, err := iox.ReadFile(testrsc.ResiliencyInterpret1)
+	if err != nil {
+		return nil, messaging.NewStatusError(messaging.StatusIOError, err, "", "")
+	}
+	status := resolver.PutContent(urn.ResiliencyInterpret, "author", buf, 1)
+	if !status.OK() {
+		return nil, status
+	}
+	buf, err = iox.ReadFile(testrsc.ResiliencyThreshold1)
+	if err != nil {
+		return nil, messaging.NewStatusError(messaging.StatusIOError, err, "", "")
+	}
+	return resolver, resolver.PutContent(urn.ResiliencyThreshold, "author", buf, 1)
 }
