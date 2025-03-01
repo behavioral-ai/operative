500ba41e79919ecd985037cc39bff6caa3434ac6
advanced-go <mark.wilson.go@outlook.com>
2025-03-01 12:08:55 -0600

    Update package.go

--- agent/package.go
+++ agent/package.go
@@ -2,10 +2,11 @@ package agent
 
 import (
 	"github.com/behavioral-ai/core/messaging"
+	"github.com/behavioral-ai/domain/collective"
 	"github.com/behavioral-ai/domain/common"
 	"github.com/behavioral-ai/operative/agent1"
 )
 
-func New(handler messaging.Agent, origin common.Origin, dispatcher messaging.Dispatcher) messaging.Agent {
-	return agent1.New(handler, origin, dispatcher)
+func New(origin common.Origin, resolver collective.Resolution, dispatcher messaging.Dispatcher) messaging.Agent {
+	return agent1.New(origin, resolver, dispatcher)
 }
